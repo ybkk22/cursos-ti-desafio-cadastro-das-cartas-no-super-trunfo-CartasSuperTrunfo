c6a6c60a801a4efc5573a31acac561c09ef5dc31
Cursos de Tecnologia da Informação <164575969+Cursos-de-Teconologia-da-Informacao@users.noreply.github.com>
2024-12-09 15:56:30 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,7 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
+//Teste larissa
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
